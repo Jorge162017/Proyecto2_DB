9e0c863052f6c8c91bcadff7f3459f5b9b44f4ef
Jorge162017 <lop221038@uvg.edu.gt>
2024-04-14 01:17:30 -0600

    Se crearon los modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'RolId',
+        'nombre',
+        'apellido',
+        'telefono',
         'email',
         'password',
+        'rol',
     ];
 
     /**
