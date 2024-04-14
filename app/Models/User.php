978875213b9857f9e68f504efd27f5f07d529562
davisBold <142925428+davisBold@users.noreply.github.com>
2024-04-14 13:40:49 -0600

    add jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class Usuario extends Authenticatable
 {
@@ -45,4 +46,16 @@ class Usuario extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    // Método para devolver un array con los claims personalizados que se añadirán al JWT.
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
